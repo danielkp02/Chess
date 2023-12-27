a25965f8e828c7fbae15b5237e65b665b701dd42
dpasternak <dkpasternak@gmail.com>
2023-12-27 11:18:38 -0600

    Initial commit

---
+++ Chess/ChessMain.py
@@ -0,0 +1,8 @@
+"""
+This is our main driver file. It will be responsible for handling user input and displaying current gamestate.
+"""
+
+import pygame as p
+from Chess import ChessEngine
+
+WIDTH = HEIGHT = 512
